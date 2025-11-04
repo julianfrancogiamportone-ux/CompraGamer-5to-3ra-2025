a38851c79ec83b1fd3d389a5f0b0395b5463d3de
destefanim71-droid <destefanim71@gmail.com>
2025-11-04 20:32:05 -0300

    carrito_entidad

--- src/main/java/com/example/CompraGamer/CompraGamerApplication.java
+++ src/main/java/com/example/CompraGamer/CompraGamerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompraGamerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CompraGamerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CompraGamerApplication.class, args);
+	}
 
 }
--- src/main/java/com/example/CompraGamer/entity/CarritoDeCompra.java
+++ src/main/java/com/example/CompraGamer/entity/CarritoDeCompra.java
@@ -1,14 +1,6 @@
 package com.example.CompraGamer.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
-import java.util.List;
+import jakarta.persistence.*;
 
 @Entity
 public class CarritoDeCompra {
@@ -16,73 +8,26 @@ public class CarritoDeCompra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long usuario_id;
     private Long orden_id;
     private Long producto_id;
-    private int cantidad;
-    
-    @OneToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
+    private Integer cantidad;
 
-    @ManyToMany
-    @JoinTable(
-        name = "carrito_producto",
-        joinColumns = @JoinColumn(name = "carrito_id"),
-        inverseJoinColumns = @JoinColumn(name = "producto_id")
-    )
-    private List<Producto> productos;
+    public CarritoDeCompra() {}
 
-    public CarritoDeCompra() {
-        super();
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public CarritoDeCompra(Long id, Long usuario_id, Long orden_id, Long producto_id, int cantidad) {
-        super();
-        this.id = id;
-        this.usuario_id = usuario_id;
-        this.orden_id = orden_id;
-        this.producto_id = producto_id;
-        this.cantidad = cantidad;
-    }
+    public Long getUsuario_id() { return usuario_id; }
+    public void setUsuario_id(Long usuario_id) { this.usuario_id = usuario_id; }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getOrden_id() { return orden_id; }
+    public void setOrden_id(Long orden_id) { this.orden_id = orden_id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getProducto_id() { return producto_id; }
+    public void setProducto_id(Long producto_id) { this.producto_id = producto_id; }
 
-    public Long getUsuario_id() {
-        return usuario_id;
-    }
-
-    public void setUsuario_id(Long usuario_id) {
-        this.usuario_id = usuario_id;
-    }
-
-    public Long getOrden_id() {
-        return orden_id;
-    }
-
-    public void setOrden_id(Long orden_id) {
-        this.orden_id = orden_id;
-    }
-
-    public Long getProducto_id() {
-        return producto_id;
-    }
-
-    public void setProducto_id(Long producto_id) {
-        this.producto_id = producto_id;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
+    public Integer getCantidad() { return cantidad; }
+    public void setCantidad(Integer cantidad) { this.cantidad = cantidad; }
 }
